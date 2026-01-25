2dfcc241cc61ad321588752465565f98e66ca071
Reqwey <linhk1606@outlook.com>
2026-01-25 10:14:14 +0800

    feat: add indicator when loading grades

--- app/src/main/java/xyz/reqwey/myshsmu/ui/screens/ScoreScreen.kt
+++ app/src/main/java/xyz/reqwey/myshsmu/ui/screens/ScoreScreen.kt
@@ -1,6 +1,9 @@
 package xyz.reqwey.myshsmu.ui.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,6 +13,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
@@ -26,6 +30,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import xyz.reqwey.myshsmu.LoginUiState
@@ -38,11 +43,12 @@ fun ScoreScreen(
 	onYearSelected: (String) -> Unit,
 	onSemesterSelected: (Int) -> Unit
 ) {
-	Column(
-		modifier = Modifier
-			.fillMaxSize()
-			.padding(horizontal = 16.dp)
-	) {
+	Box(modifier = Modifier.fillMaxSize()) {
+		Column(
+			modifier = Modifier
+				.fillMaxSize()
+				.padding(horizontal = 16.dp)
+		) {
 		// Dropdowns Row
 		Row(
 			modifier = Modifier.fillMaxWidth(),
@@ -179,6 +185,18 @@ fun ScoreScreen(
 				HorizontalDivider(modifier = Modifier.padding(horizontal = 4.dp), thickness = 0.5.dp)
 			}
 		}
+	}
+
+	if (uiState.isLoading) {
+		Box(
+			modifier = Modifier
+				.fillMaxSize()
+				.background(Color.Black.copy(alpha = 0.3f)),
+			contentAlignment = Alignment.Center
+		) {
+			CircularProgressIndicator()
+		}
+	}
 	}
 }
 
